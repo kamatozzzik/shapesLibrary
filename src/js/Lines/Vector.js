e57456f293a5cb092adede381a5fc1440cfadcb1
Vladislav <mycash_crpt@mail.ru>
2019-03-12 09:33:42 +0300

    add lines add shapes

--- src/js/Lines/Vector.js
+++ src/js/Lines/Vector.js
@@ -1,18 +1,10 @@
 import { Line } from './Line';
 
 export class Vector extends Line {
-	startDraw(ctx) {
+	render(ctx) {
 		ctx.beginPath();
 		ctx.moveTo(this.fromX, this.fromY);
-	}
-
-	endDraw(ctx) {
 		ctx.lineTo(this.toX, this.toY);
 		ctx.stroke();
-	}
-
-	render(ctx) {
-		this.startDraw(ctx);
-		this.endDraw(ctx);
 	}
 }
--- src/startApp.js
+++ src/startApp.js
@@ -9,37 +9,55 @@ canvasNode.addEventListener('mousemove', e => {
 	if (app.currentShape) {
 		app.currentShape.setPosition(e.clientX, e.clientY);
 	}
+	if (app.currentLine) {
+		app.currentLine.setToPos(e.clientX, e.clientY);
+	}
 });
 
-// Установка фигуры на экране
+// Установка фигур на экране
 canvasNode.addEventListener('click', e => {
+	let currentX = e.clientX,
+		currentY = e.clientY;
 	if (
 		app.currentShape &&
-		app.checkPositions(e.clientX, e.clientY, styles.currentDistance)
+		app.checkPositions(currentX, currentY, styles.currentDistance)
 	) {
 		const shapeClass = app.currentShape.constructor;
 		app.addShape(app.currentShape);
-		app.addPosition(e.clientX, e.clientY);
-		app.setCurrentShape(createShape(shapeClass, e.clientX, e.clientY));
+		app.addPosition(currentX, currentY);
+		app.setCurrentShape(createShape(shapeClass, currentX, currentY));
+	}
+
+	if (app.currentLine) {
+		if (app.lineFromPos.length > app.lineToPos.length) {
+			app.addToPos(currentX, currentY);
+			app.currentLine.setToPos(currentX, currentY);
+
+			let lineClass = app.currentLine.constructor;
+			app.lines.push(app.currentLine);
+			app.setCurrentLine(createLine(lineClass));
+		} else {
+			app.addFromPos(currentX, currentY);
+			app.currentLine.setFromPos(currentX, currentY);
+		}
 	}
 });
 
-// Установка фигуры на курсор
+// Установка фигуры
 document.addEventListener('click', e => {
 	const shape = e.target.dataset.shape;
 	if (shape && shapeMap.hasOwnProperty(shape)) {
+		app.setCurrentLine(null);
 		app.setCurrentShape(createShape(shapeMap[shape]));
 	}
-});
-
-// Сброс текущей фигуры
-document.addEventListener('keydown', e => {
-	if (e.keyCode === 27) {
+	const line = e.target.dataset.line;
+	if (line && lineMap.hasOwnProperty(line)) {
 		app.setCurrentShape(null);
+		app.setCurrentLine(createLine(lineMap[line]));
 	}
 });
 
-// Задание параметров фигуре
+///////////////------------------ Shapes-------------------------////////////
 function createShape(ShapeClass, x, y) {
 	const shape = new ShapeClass(x, y, styles.currentSize);
 	shape.setFillColor(styles.currentFillColor);
@@ -48,17 +66,7 @@ function createShape(ShapeClass, x, y) {
 	return shape;
 }
 
-window.addEventListener('load', handlerResize);
-window.addEventListener('resize', handlerResize);
-
-// При ресайзе изменяется ширина окна canvas
-function handlerResize() {
-	canvasNode.width = window.innerWidth;
-	canvasNode.height = window.innerHeight;
-}
-
 ///////////////------------------ LINES -------------------------////////////
-
 function createLine(LineClass) {
 	const line = new LineClass();
 
@@ -67,27 +75,19 @@ function createLine(LineClass) {
 	return line;
 }
 
-document.addEventListener('click', e => {
-	const line = e.target.dataset.line;
-	if (line && lineMap.hasOwnProperty(line)) {
-		app.setCurrentLine(createLine(lineMap[line]));
+// Сброс текущей фигуры
+document.addEventListener('keydown', e => {
+	if (e.keyCode === 27) {
+		app.setCurrentShape(null);
+		app.setCurrentLine(null);
 	}
 });
 
-canvasNode.addEventListener('click', e => {
-	if (app.currentLine) {
-		if (app.lineFromPos.length > app.lineToPos.length) {
-			app.addToPos(e.clientX, e.clientY);
-			app.currentLine.setToPos(e.clientX, e.clientY);
-			let lineClass = app.currentLine.constructor;
-			app.lines.push(app.currentLine);
-			app.setCurrentLine(createLine(lineClass));
-			console.log('Установили позицию to');
-		} else {
-			app.addFromPos(e.clientX, e.clientY);
-			app.currentLine.setFromPos(e.clientX, e.clientY);
-			console.log('Установили позицию from');
-			console.log(app.lines);
-		}
-	}
-});
+// При ресайзе изменяется ширина окна canvas
+function handlerResize() {
+	canvasNode.width = window.innerWidth;
+	canvasNode.height = window.innerHeight;
+}
+
+window.addEventListener('load', handlerResize);
+window.addEventListener('resize', handlerResize);
